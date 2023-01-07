8658bc7b45e032fb2a1b7e027c384b5f6853191e
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-01-08 02:21:14 +0900

    chore - #4: Tailwind CSS 설치 및  기본 세팅

---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js, jsx, ts, tsx}',
+    './components/**/*.{js, jsx, ts, tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans KR', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
