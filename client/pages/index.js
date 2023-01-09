cf4be7804e8ec576f30177ffece569af4af9bb6f
hasuhwan <hapung215@gmail.com>
2023-01-09 22:42:11 +0900

    feat - #8: BottomNavBar, DropDown, AuthArticle, ReviewArticle UI 구현

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,9 @@
+import { ReviewArticle } from '../components/reviewArticle';
 import { AuthArticle } from '../components/authArticle';
 export default function Home() {
   return (
     <>
+      <ReviewArticle />
       <AuthArticle />
     </>
   );
--- client/pages/search.js
+++
@@ -1,7 +0,0 @@
-export default function Search() {
-  return (
-    <>
-      <div>hi</div>
-    </>
-  );
-}
