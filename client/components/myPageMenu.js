14ed4896db111ed6566de3369a2a5eadf14bfa9e
Menat91 <jurilover@naver.com>
2023-01-11 11:23:43 +0900

    design - 인증서발급 div 목업

--- client/components/myPageMenu.js
+++ client/components/myPageMenu.js
@@ -1,11 +1,10 @@
 // <------  사용하시기 전에 꼭 읽어주세요! ------>
-// ToDo 1. 버튼을 클릭했을 때의 Click Event를 onClick={}으로 넣어주셔야 합니다.
-// ToDo 2. 버튼의 이름을 buttonName으로 넣어주셔야 합니다.
+// ToDo 1.
 
 // <예시> 아래의 코드를 주석 해제하고 중괄호를 제거한 뒤 사용해보세요
 
 {
-  /* <WantedHabit
+  /* <MyPageMenuList
   onClick={() => console.log('버튼의 클릭이벤트로 전해지는 함수')}
 /> */
 }
@@ -15,31 +14,13 @@
 import { SlArrowRight } from 'react-icons/sl';
 
 export const MyPageMenuList = (props) => {
-  const MyPageUserInfo = (props) => {
+  const CertificationList = (props) => {
     return (
-      <div>
-        <div className="w-80 cursor-pointer" onClick={props.onClick}>
-          <div className="p-2 font-bold  w-full only:flex place-content-between border-x-2 border-t-2 border-black dark:text-white dark:border-white">
-            회원 정보 수정
-            <div className="pr-5">
-              <SlArrowRight className="inline align-middle dark:bg-white" />
-            </div>
-          </div>
-        </div>
-        <div className="w-80 cursor-pointer" onClick={props.onClick}>
-          <div className="p-2 font-bold  w-full only:flex place-content-between border-x-2 border-t-2 border-black dark:text-white dark:border-white">
-            로그아웃
-            <div className="pr-5">
-              <SlArrowRight className="inline align-middle dark:bg-white" />
-            </div>
-          </div>
-        </div>
-        <div className="w-80 cursor-pointer" onClick={props.onClick}>
-          <div className="p-2 font-bold  w-full only:flex place-content-between border-x-2 border-t-2 border-black dark:text-white dark:border-white">
-            회원탈퇴
-            <div className="pr-5">
-              <SlArrowRight className="inline align-middle dark:bg-white" />
-            </div>
+      <div className="w-80 cursor-pointer hidden" onClick={props.onClick}>
+        <div className="p-2 font-bold  w-full only:flex place-content-between border-x-2 border-t-2 border-black dark:text-white dark:border-white">
+          숨겨진 업적
+          <div className="pr-5">
+            <SlArrowRight className="inline align-middle dark:bg-white" />
           </div>
         </div>
       </div>
@@ -72,6 +53,7 @@ export const MyPageMenuList = (props) => {
           </div>
         </div>
       </div>
+      <CertificationList />
       <div className="w-80 cursor-pointer" onClick={props.onClick}>
         <div className="p-2 font-bold  w-full only:flex place-content-between border-x-2 border-t-2 border-black dark:text-white dark:border-white">
           친구 초대
@@ -81,7 +63,7 @@ export const MyPageMenuList = (props) => {
         </div>
       </div>
       <div className="w-80 cursor-pointer" onClick={props.onClick}>
-        <div className="p-2 font-bold  w-full only:flex place-content-between border-x-2 border-t-2 border-black dark:text-white dark:border-white">
+        <div className="p-2 font-bold  w-full only:flex place-content-between border-2 border-black dark:text-white dark:border-white">
           고객센터
           <div className="pr-5">
             <SlArrowRight className="inline align-middle dark:bg-white" />
@@ -89,7 +71,32 @@ export const MyPageMenuList = (props) => {
         </div>
       </div>
 
-      <MyPageUserInfo />
+      <div className="my-3">
+        <div className="w-80 cursor-pointer" onClick={props.onClick}>
+          <div className="p-2 font-bold  w-full only:flex place-content-between border-x-2 border-t-2 border-black dark:text-white dark:border-white">
+            회원 정보 수정
+            <div className="pr-5">
+              <SlArrowRight className="inline align-middle dark:bg-white" />
+            </div>
+          </div>
+        </div>
+        <div className="w-80 cursor-pointer" onClick={props.onClick}>
+          <div className="p-2 font-bold  w-full only:flex place-content-between border-x-2 border-t-2 border-black dark:text-white dark:border-white">
+            로그아웃
+            <div className="pr-5">
+              <SlArrowRight className="inline align-middle dark:bg-white" />
+            </div>
+          </div>
+        </div>
+        <div className="w-80 cursor-pointer" onClick={props.onClick}>
+          <div className="p-2 font-bold  w-full only:flex place-content-between border-2 border-black dark:text-white dark:border-white">
+            회원탈퇴
+            <div className="pr-5">
+              <SlArrowRight className="inline align-middle dark:bg-white" />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
