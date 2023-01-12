714a68e66e30bdc7b22e68b2a775eb8cb6e60a3a
Rbaam <kimdg0814k@gmail.com>
2023-01-13 07:57:19 +0900

    feat #7: Haeder 컴포넌트 완료

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,10 +2,12 @@ import '../styles/globals.css';
 
 import { BottomNav } from '../components/bottomNav/bottomNav';
 import { HomeNav } from '../components/homeNav';
+import { Header } from '../components/header';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Header />
       <HomeNav />
       <main className="pb-[100px]">
         <Component {...pageProps} />
