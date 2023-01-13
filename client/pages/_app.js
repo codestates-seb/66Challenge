7255f57ad0188a2b7713ac68195decab6322f55e
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-01-13 09:03:45 +0900

    feat: RTK를 활용하여 전역 상태 관리 로직 구현 (1차) (#84)
    * docs: Update issue templates
    
    * feat - #1: Login UI 구현 완료, CSS 조정 필요
    
    * feat - #64: Home Page UI 구현
    
    * feat - #65: Habit Post UI 구현 1차
    
    * feat - #65: Habit Post UI 구현 1차
    
    * feat - #65: Habit Post Page UI 구현
    
    * feat - #81: feat: RTK를 활용하여 전역 상태 관리 로직 구현(1차)

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,16 +2,18 @@ import '../styles/globals.css';
 
 import { BottomNav } from '../components/bottomNav/bottomNav';
 import { HomeNav } from '../components/homeNav';
+import { Provider } from 'react-redux';
+import store from '../ducks/store';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <Provider store={store}>
       <HomeNav />
       <main className="pb-[100px]">
         <Component {...pageProps} />
       </main>
       <BottomNav />
-    </>
+    </Provider>
   );
 }
 
