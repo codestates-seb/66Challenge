f39e01e3352e0b33679a6303297d6d1e0d6a5a1f
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-01-13 10:43:22 +0900

    docs: Update issue templates (#85)
    Co-authored-by: jwParkDev <110954895+jwParkDev@users.noreply.github.com>
    Co-authored-by: hasuhwan <hapung215@gmail.com>

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -2,19 +2,18 @@ import '../styles/globals.css';
 
 import { BottomNav } from '../components/bottomNav/bottomNav';
 import { HomeNav } from '../components/homeNav';
-import { Provider } from 'react-redux';
-import store from '../ducks/store';
-
+import { wrapper, persistor } from '../ducks/store';
+import { PersistGate } from 'redux-persist/integration/react';
 function MyApp({ Component, pageProps }) {
+  console.log(wrapper);
   return (
-    <Provider store={store}>
+    <PersistGate persistor={persistor} loading={<div>loading...</div>}>
       <HomeNav />
       <main className="pb-[100px]">
         <Component {...pageProps} />
       </main>
       <BottomNav />
-    </Provider>
+    </PersistGate>
   );
 }
-
-export default MyApp;
+export default wrapper.withRedux(MyApp);
