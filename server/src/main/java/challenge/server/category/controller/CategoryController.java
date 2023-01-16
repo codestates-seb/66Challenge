f3b68edb180cbb727ae6ebcdad8e8b8f5c31653d
YerimSW <swyr1016@gmail.com>
2023-01-16 17:50:14 +0900

    docs - #18: 현재 존재하지 않는 엔드포인트 주석처리

--- server/src/main/java/challenge/server/category/controller/CategoryController.java
+++ server/src/main/java/challenge/server/category/controller/CategoryController.java
@@ -20,42 +20,43 @@ import java.util.List;
 @RequestMapping("/categories")
 public class CategoryController {
 
-    @ApiOperation(value = "카테고리 생성", notes = "관리자가 카테고리를 등록합니다.")
-    @PostMapping
-    public ResponseEntity createCategory(@RequestBody @Valid CategoryDto.Post categoryPostDto){
-        CategoryDto.Response response = createCategoryResponseDto();
-        return new ResponseEntity(response, HttpStatus.CREATED);
-    }
-
-    @ApiOperation(value = "카테고리 수정", notes="카테고리의 이름을 수정합니다.")
-    @PatchMapping("/{category-id}")
-    public ResponseEntity changeCategory(@PathVariable("category-id") @Positive Long categoryId) {
-        CategoryDto.Response response = createCategoryResponseDto();
-        return new ResponseEntity(response, HttpStatus.OK);
-    }
-
-    @ApiOperation(value = "모든 카테고리 조회")
-    @GetMapping
-    public ResponseEntity findCategories(@RequestParam @Positive int page,
-                                         @RequestParam @Positive int size) {
-        List<CategoryDto.Response> responses = List.of(createCategoryResponseDto(),createCategoryResponseDto());
-        return new ResponseEntity(responses, HttpStatus.OK);
-    }
-
-    @ApiOperation(value = "카테고리 삭제")
-    @DeleteMapping("/{category-id}")
-    public ResponseEntity deleteCategory(@PathVariable("category-id") @Positive Long categoryId) {
-
-        return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
-
-    // 응답 더미데이터 - 카테고리 DTO
-    public CategoryDto.Response createCategoryResponseDto() {
-        return CategoryDto.Response.builder()
-                .categoryId(1L)
-                .type("자기계발")
-                .build();
-    }
+//    TODO 카테고리 CRUD 관리자 페이지 구현 시 필요
+//    @ApiOperation(value = "카테고리 생성", notes = "관리자가 카테고리를 등록합니다.")
+//    @PostMapping
+//    public ResponseEntity createCategory(@RequestBody @Valid CategoryDto.Post categoryPostDto){
+//        CategoryDto.Response response = createCategoryResponseDto();
+//        return new ResponseEntity(response, HttpStatus.CREATED);
+//    }
+//
+//    @ApiOperation(value = "카테고리 수정", notes="카테고리의 이름을 수정합니다.")
+//    @PatchMapping("/{category-id}")
+//    public ResponseEntity changeCategory(@PathVariable("category-id") @Positive Long categoryId) {
+//        CategoryDto.Response response = createCategoryResponseDto();
+//        return new ResponseEntity(response, HttpStatus.OK);
+//    }
+//
+//    @ApiOperation(value = "모든 카테고리 조회")
+//    @GetMapping
+//    public ResponseEntity findCategories(@RequestParam @Positive int page,
+//                                         @RequestParam @Positive int size) {
+//        List<CategoryDto.Response> responses = List.of(createCategoryResponseDto(),createCategoryResponseDto());
+//        return new ResponseEntity(responses, HttpStatus.OK);
+//    }
+//
+//    @ApiOperation(value = "카테고리 삭제")
+//    @DeleteMapping("/{category-id}")
+//    public ResponseEntity deleteCategory(@PathVariable("category-id") @Positive Long categoryId) {
+//
+//        return new ResponseEntity(HttpStatus.NO_CONTENT);
+//    }
+//
+//    // 응답 더미데이터 - 카테고리 DTO
+//    public CategoryDto.Response createCategoryResponseDto() {
+//        return CategoryDto.Response.builder()
+//                .categoryId(1L)
+//                .type("자기계발")
+//                .build();
+//    }
 
 }
 
--- server/src/main/java/challenge/server/report/dto/ReportDto.java
+++ server/src/main/java/challenge/server/report/dto/ReportDto.java
@@ -8,32 +8,49 @@ public class ReportDto {
     @ApiModel(value = "신고 등록 요청 시 전달")
     @Getter
     @Setter
-    public static class Post {
-        @ApiModelProperty(example = "Review")
-        private String postType;
+    public static class HabitPost {
         @ApiModelProperty(example = "1", value = "게시물 식별자")
-        private Long postId;
+        private Long habitId;
         @ApiModelProperty(example = "1", value = "유저(신고자) 식별자")
         private Long reporter;
         @ApiModelProperty(example = "ABUSE")
         private String reportType;
     }
 
-    @ApiModel(value = "신고 조회 요청 시 전달")
-    @Getter
-    @NoArgsConstructor
-    @Builder
-    @AllArgsConstructor
-    public static class Response {
-        @ApiModelProperty(example = "1", value = "신고 식별자")
-        private Long reportId;
-        @ApiModelProperty(example = "Review")
-        private String postType;
+    public static class AuthPost {
         @ApiModelProperty(example = "1", value = "게시물 식별자")
-        private Long postId;
+        private Long authId;
         @ApiModelProperty(example = "1", value = "유저(신고자) 식별자")
         private Long reporter;
         @ApiModelProperty(example = "ABUSE")
         private String reportType;
     }
+
+    public static class ReviewPost {
+        @ApiModelProperty(example = "1", value = "게시물 식별자")
+        private Long reviewId;
+        @ApiModelProperty(example = "1", value = "유저(신고자) 식별자")
+        private Long reporter;
+        @ApiModelProperty(example = "ABUSE")
+        private String reportType;
+    }
+
+//    TODO 전체 신고 조회 기능 구현 시 필요
+//    @ApiModel(value = "신고 조회 요청 시 전달")
+//    @Getter
+//    @NoArgsConstructor
+//    @Builder
+//    @AllArgsConstructor
+//    public static class Response {
+//        @ApiModelProperty(example = "1", value = "신고 식별자")
+//        private Long reportId;
+//        @ApiModelProperty(example = "Review")
+//        private String postType;
+//        @ApiModelProperty(example = "1", value = "게시물 식별자")
+//        private Long postId;
+//        @ApiModelProperty(example = "1", value = "유저(신고자) 식별자")
+//        private Long reporter;
+//        @ApiModelProperty(example = "ABUSE")
+//        private String reportType;
+//    }
 }
