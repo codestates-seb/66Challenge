0148188332840dea687153a14146a7f05ce3869c
Rbaam <111344611+Rbaam@users.noreply.github.com>
2023-01-19 10:16:49 +0900

    Revert "refactor - #138: MyPage 리팩터링 완료 (#156)"
    This reverts commit fd615063e00960f183c8cb929336ffd9e3c589a9.

--- client/next.config.js
+++ client/next.config.js
@@ -1,37 +1,6 @@
 /** @type {import('next').NextConfig} */
-
-const widthPlugins = require('next-compose-plugins');
-const withPWA = require('next-pwa')({
-  dest: 'public',
-});
-
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
+}
 
-    return config;
-  },
-};
-
-const remoteImg = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'media.istockphoto.com',
-      },
-    ],
-  },
-};
-
-module.exports = widthPlugins([nextConfig, withPWA, remoteImg]);
-
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-// });
-
-// module.exports = withPWA({});
+module.exports = nextConfig
