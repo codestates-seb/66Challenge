900d79f3152f5dd9c6b4b23026067da2e97cb32f
Rbaam <111344611+Rbaam@users.noreply.github.com>
2023-01-19 10:16:49 +0900

    Revert "refactor - #138: MyPage 리팩터링 완료 (#156)"
    This reverts commit fd615063e00960f183c8cb929336ffd9e3c589a9.

--- client/pages/habit/search/[keyword].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-
-const Search = () => {
-  const router = useRouter();
-  console.log(router);
-  return <div>i</div>;
-};
-
-export default Search;
---
+++ client/pages/index.js
@@ -0,0 +1,5 @@
+export default function Home() {
+  const a = 'd';
+
+  return <></>;
+}
