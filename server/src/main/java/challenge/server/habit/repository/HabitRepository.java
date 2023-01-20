ac5f64b69763307061859663273169c3db42bc84
Eunyoung KANG <greenkey20@gmail.com>
2023-01-20 09:44:20 +0900

    feat - #25 #28: UserService관련 HabitRepo내용추가
    Related to: #25 #28

--- server/src/main/java/challenge/server/habit/repository/HabitRepository.java
+++ server/src/main/java/challenge/server/habit/repository/HabitRepository.java
@@ -9,4 +9,7 @@ public interface HabitRepository extends JpaRepository<Habit, Long> {
     Page<Habit> findByTitleIsContaining(String keyword, Pageable pageable);
     Page<Habit> findByCategoryCategoryId(Long categoryId, Pageable pageable);
     Page<Habit> findByHostUserId(Long userId, Pageable pageable);
+
+    // User 마이페이지로부터 '내가 만든 습관 조회' 관련 추가
+    Page<Habit> findAllByHostUserId(Long userId, Pageable pageable);
 }
