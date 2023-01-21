c4481f27b004cc39621aa8fed266eebf9b704f20
Eunyoung KANG <greenkey20@gmail.com>
2023-01-21 10:09:39 +0900

    fix - #25: 회원 탈퇴 기능 보완
    Related to: #25

---
+++ server/src/main/java/challenge/server/user/entity/UserRole.java
@@ -0,0 +1,22 @@
+//package challenge.server.user.entity;
+//
+//
+//import challenge.server.audit.BaseTimeEntity;
+//import lombok.AllArgsConstructor;
+//import lombok.Builder;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//import javax.persistence.*;
+//
+//@Builder
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Table(name = "USER_ROLES")
+//@Entity
+//public class UserRole extends BaseTimeEntity {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long userRoleId;
+//}
