db8ee62dee50d40906ef96b4cc1b2f1522694c04
jambin <gksmfcksqls@gmail.com>
2023-01-24 16:41:13 +0900

    feat: OAuth2 redirect-uri 수정

--- server/src/main/java/challenge/server/security/oauth/dto/SessionUser.java
+++ server/src/main/java/challenge/server/security/oauth/dto/SessionUser.java
@@ -1,22 +1,22 @@
-package challenge.server.security.oauth.dto;
-
-import challenge.server.user.entity.User;
-import lombok.Getter;
-
-import java.io.Serializable;
-
-/**
- * 직렬화 기능을 가진 User 클래스
- * User 클래스에 직렬화하지 않는 이유는 다른 엔티티와도 연관관계가 생길 수 있는 엔티티이기 때문에 성능 이슈, 사이드 이펙트가 발생할 확률이 높기 때문이다.
- */
-@Getter
-public class SessionUser implements Serializable {
-    private String username;
-    private String email;
-    private String profileImageUrl;
-
-    public SessionUser(User user) {
-        this.username = user.getUsername();
-        this.email = user.getEmail();
-    }
-}
+//package challenge.server.security.oauth.dto;
+//
+//import challenge.server.user.entity.User;
+//import lombok.Getter;
+//
+//import java.io.Serializable;
+//
+///**
+// * 직렬화 기능을 가진 User 클래스
+// * User 클래스에 직렬화하지 않는 이유는 다른 엔티티와도 연관관계가 생길 수 있는 엔티티이기 때문에 성능 이슈, 사이드 이펙트가 발생할 확률이 높기 때문이다.
+// */
+//@Getter
+//public class SessionUser implements Serializable {
+//    private String username;
+//    private String email;
+//    private String profileImageUrl;
+//
+//    public SessionUser(User user) {
+//        this.username = user.getUsername();
+//        this.email = user.getEmail();
+//    }
+//}
