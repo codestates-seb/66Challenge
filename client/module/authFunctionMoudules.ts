d0521f201fce73e7c04a912f0c6ce05eb1eda8d5
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-01-25 16:15:36 +0900

    feat - #195: 하단 네비게이션 데이터 상태에 따른 분기 및 인증 페이지 데이터 전송 구현 (#204)
    * docs: Update issue templates
    
    * 병합
    
    * Merge remote-tracking branch 'upstream/devFront'
    
    * 병합
    
    * 머지 하려고 함
    
    Co-authored-by: jwParkDev <110954895+jwParkDev@users.noreply.github.com>
    Co-authored-by: hasuhwan <hapung215@gmail.com>

--- client/module/authFunctionMoudules.ts
+++ client/module/authFunctionMoudules.ts
@@ -1,16 +1,3 @@
 import axios from 'axios';
 import { getCookie } from './cookies';
 import { getAuthProps } from './moduleInterface';
-
-export async function getHabitAuths({ habitId, page, size }: getAuthProps) {
-  try {
-    const response = await axios
-      .get(
-        `${process.env.NEXT_PUBLIC_SERVER_URL}/habits/${habitId}/auths?page=${page}&size=${size}`,
-      )
-      .then((res) => console.log(res));
-    return response;
-  } catch (e) {
-    console.error(e);
-  }
-}
