c82fd5da00da112e8d599bd549a4e206f91a8d06
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-01-25 09:07:01 +0900

    fix - #166: Habit 관련 Comp & Page Ajax 연결 관련 코드 수정

--- client/pages/habit/detail/[habitId]/auth.tsx
+++ client/pages/habit/detail/[habitId]/auth.tsx
@@ -1,23 +1,23 @@
 import { useRouter } from 'next/router';
 import { AuthArticle } from '../../../../components/authArticle';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useIntersection } from '../../../../hooks/useIntersection';
 import type { authArticleProps } from '../../../../components/authArticle';
 
 export default function HabitDetailAuth() {
   const router = useRouter();
   const habitId = router.query.habitId;
-  const url: string = '';
+  const url: string = `${process.env.NEXT_PUBLIC_SERVER_URL}/habits/${habitId}/auths?`;
   const [authData, setAuthData] = useState<Array<authArticleProps>>([]);
-  const [page, setPage] = useState(0);
+  const [page, setPage] = useState(1);
   const [setTarget] = useIntersection(url, page, setPage, setAuthData);
 
   return (
-    <div className="habit-detail-review-container px-5">
+    <div className="habit-detail-auth-container px-5">
       {authData.map((el) => {
         return (
-          <div className="habit-detail-review-wrapper">
-            <AuthArticle {...el} />
+          <div className="habit-detail-auth-wrapper" key={el.authId}>
+            <AuthArticle {...el} habitId={+habitId} />
           </div>
         );
       })}
