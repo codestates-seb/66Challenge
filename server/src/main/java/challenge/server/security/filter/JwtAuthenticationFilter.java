7f0f9d3ed223883800e1937aa04f26d80c96d93f
Eunyoung KANG <greenkey20@gmail.com>
2023-01-28 02:08:09 +0900

    fix - #11: 로그인 로직 수정
    Related to: #11

--- server/src/main/java/challenge/server/security/filter/JwtAuthenticationFilter.java
+++ server/src/main/java/challenge/server/security/filter/JwtAuthenticationFilter.java
@@ -1,5 +1,6 @@
 package challenge.server.security.filter;
 
+import challenge.server.security.dto.LoginDto;
 import challenge.server.security.jwt.JwtTokenizer;
 import challenge.server.security.user.dto.UserDto;
 import challenge.server.security.user.entity.User;
