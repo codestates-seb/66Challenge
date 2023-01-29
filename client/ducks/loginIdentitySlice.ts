5bfb2bf0039f404560e276d792a7765d152986de
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-01-29 18:13:18 +0900

    feat - #283: 카카오 쉐어 주소 수정 및 파이어베이스 토큰 발급 컴포넌트 제작 (#284)
    * docs: Update issue templates
    
    * 병합
    
    * Merge remote-tracking branch 'upstream/devFront'
    
    * 병합
    
    * feat - #283: 카카오 쉐어 주소 수정 및 파이어베이스 토큰 발급 컴포넌트 제작
    
    ---------
    
    Co-authored-by: jwParkDev <110954895+jwParkDev@users.noreply.github.com>
    Co-authored-by: hasuhwan <hapung215@gmail.com>

--- client/ducks/loginIdentitySlice.ts
+++ client/ducks/loginIdentitySlice.ts
@@ -36,10 +36,12 @@ const loginRequest = createAsyncThunk(
 interface loginIdentity {
   isLogin: boolean;
   userId: number | null;
+  notificationToken: string | null;
 }
 const initialState: loginIdentity = {
   isLogin: false,
   userId: null,
+  notificationToken: null,
 };
 
 export const loginIdentitySlice = createSlice({
@@ -49,11 +51,15 @@ export const loginIdentitySlice = createSlice({
     initLoginIdentity: (state: loginIdentity): void => {
       state.isLogin = false;
       state.userId = null;
+      state.notificationToken = null;
       removeCookie('accessJwtToken');
     },
     oauthLogin: (state, action): void => {
       state.isLogin = true;
       state.userId = action.payload;
+    },
+    notificationToken: (state, action: PayloadAction<string>): void => {
+      state.notificationToken = action.payload;
     },
   },
   extraReducers: (builder): void => {
@@ -66,5 +72,6 @@ export const loginIdentitySlice = createSlice({
     );
   },
 });
-export const { initLoginIdentity, oauthLogin } = loginIdentitySlice.actions;
+export const { initLoginIdentity, oauthLogin, notificationToken } =
+  loginIdentitySlice.actions;
 export { loginRequest };
