846d76d98a3b3533bc6048f55b6ab5a0054e9fa2
Eunyoung KANG <greenkey20@gmail.com>
2023-01-30 00:59:23 +0900

    fix - #11: User 나이 기본 값(0) 설정

--- server/src/main/java/challenge/server/user/entity/UserStatistics.java
+++ server/src/main/java/challenge/server/user/entity/UserStatistics.java
@@ -10,4 +10,15 @@ public class UserStatistics {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userStatisticsId;
+
+//    private int numOfChallenges; // 내가 참여중인 습관 갯수(프론트)
+//    private int numOfBookmarks; // 찜한 습관 갯수(프론트)
+//    private int numOfHostHabits; // 만든 습관 갯수(프론트)
+
+    // 내가 현재까지 올린 인증 글의 갯수 -> todo 습관별? 전체?
+    private int numOfAuthByHabit = 0;
+
+
+
+
 }
