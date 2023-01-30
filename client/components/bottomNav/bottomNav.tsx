df8f174d0dcf0ddf824d6555ab35411318f35b5a
hasuhwan <107896191+hasuhwan@users.noreply.github.com>
2023-01-31 01:04:32 +0900

    feat - #338: 세로스크롤 막히는 거 해결 (#339)
    * docs: Update issue templates
    
    * 병합
    
    * Merge remote-tracking branch 'upstream/devFront'
    
    * 병합
    
    * feat - #338: 세로스크롤 막히는 거 해결
    
    ---------
    
    Co-authored-by: jwParkDev <110954895+jwParkDev@users.noreply.github.com>
    Co-authored-by: hasuhwan <hapung215@gmail.com>

--- client/components/bottomNav/bottomNav.tsx
+++ client/components/bottomNav/bottomNav.tsx
@@ -1,6 +1,7 @@
 import { MainBottomNav } from './mainBottomNav';
 import { HabitPageBottomNav } from './habitPageBottomNav';
 import { useRouter } from 'next/router';
+import { useEffect, useCallback } from 'react';
 
 export function BottomNav() {
   //라우팅 된 페이지 경로에 따른 조건 부 렌더링 작성해야함.
@@ -8,6 +9,14 @@ export function BottomNav() {
   const pathname: string = router.asPath;
   //경로 조건에 따른 nav bar 다르게 해야함. 페이지 작성 다되고나면 분할 예정
   // 로그인 페이지 회원가입 페이지는 nav bar 존재하지 않는다.
+  const openScroll = useCallback(() => {
+    if (document.body.style.overflow === 'hidden') {
+      document.body.style.removeProperty('overflow');
+    }
+  }, []);
+  useEffect(() => {
+    openScroll();
+  }, [pathname]);
   const paramArr: string[] = pathname.split('/');
   let param: string = '';
   if (pathname.includes('detail')) {
