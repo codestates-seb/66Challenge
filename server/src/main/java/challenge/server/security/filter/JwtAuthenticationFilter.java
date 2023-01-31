177668de6fe4e25e9ecaa9e2a80b31f9042be94e
Eunyoung KANG <greenkey20@gmail.com>
2023-02-01 02:31:02 +0900

    fix - Spring Security config 등 수정

--- server/src/main/java/challenge/server/security/filter/JwtAuthenticationFilter.java
+++ server/src/main/java/challenge/server/security/filter/JwtAuthenticationFilter.java
@@ -23,7 +23,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
