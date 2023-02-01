c87d44ec82554c1dde85dd105f5660298cbca7b1
Eunyoung KANG <greenkey20@gmail.com>
2023-02-01 16:39:52 +0900

    fix - Spring Security 설정 변경

---
+++ server/src/main/java/challenge/server/config/UserSecurityConfig.java
@@ -0,0 +1,14 @@
+//package challenge.server.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//public class UserSecurityConfig  extends WebSecurityConfigurerAdapter {
+//    @Bean
+//    public AuthenticationManager getAuthenticationManager() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//}
