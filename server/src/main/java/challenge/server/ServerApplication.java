dc29b787da965a88a328d18911e211e95d7f7e19
jambin <gksmfcksqls@gmail.com>
2023-02-01 18:51:51 +0900

    feat: 톰캣 서버 시간 변경(Asia/Seoul)

--- server/src/main/java/challenge/server/ServerApplication.java
+++ server/src/main/java/challenge/server/ServerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @EnableScheduling
 @SpringBootApplication
@@ -16,6 +19,15 @@ public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	/**
+	 * Bean이 완전히 초기화 된 후, 단 한 번만 호출되는 메서드.
+	 * EC2에서도 Tomcat 서버의 시간을 서울 시간으로 변경한다.
+	 */
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
