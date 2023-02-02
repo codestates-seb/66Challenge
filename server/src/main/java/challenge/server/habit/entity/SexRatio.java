9eea2854aa8029b51916b2ab5f602659f46cc86b
jambin <gksmfcksqls@gmail.com>
2023-02-03 00:06:10 +0900

    feat: habit 통계 데이터 전달

--- server/src/main/java/challenge/server/habit/entity/SexRatio.java
+++ server/src/main/java/challenge/server/habit/entity/SexRatio.java
@@ -12,31 +12,31 @@ import static challenge.server.user.entity.User.Gender.MALE;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SexRatio {
     @ColumnDefault(value = "0")
-    int maleCount;
+    int male;
 
     @ColumnDefault(value = "0")
-    int femaleCount;
-
-    public SexRatio makeStatistics(Habit habit) {
-        this.femaleCount = (int) habit.getChallenges().stream()
-                .filter(challenge -> challenge.getUser().getGender() == FEMALE).count();
-        this.maleCount = (int) habit.getChallenges().stream()
-                .filter(challenge -> challenge.getUser().getGender() == MALE).count();
-
-        return this;
-    }
-
-//    public SexRatio makeStatistics(Habit habit) {
-//        if (habit != null) {
-//            int totalChallengeCount = habit.getChallenges().size();
-//            double maleCount = habit.getChallenges().stream().
-//                    filter(challenge -> challenge.getUser().getGender() == MALE).count();
-//            double femaleCount = habit.getChallenges().stream().
-//                    filter(challenge -> challenge.getUser().getGender() == FEMALE).count();
+    int female;
 //
-//            this.male = (int) Math.round(maleCount / totalChallengeCount * 100);
-//            this.female = (int) Math.round(femaleCount / totalChallengeCount * 100);
-//        }
+//    public SexRatio makeStatistics(Habit habit) {
+//        this.femaleCount = (int) habit.getChallenges().stream()
+//                .filter(challenge -> challenge.getUser().getGender() == FEMALE).count();
+//        this.maleCount = (int) habit.getChallenges().stream()
+//                .filter(challenge -> challenge.getUser().getGender() == MALE).count();
+//
 //        return this;
 //    }
+
+    public SexRatio makeStatistics(Habit habit) {
+        if (habit != null) {
+            int totalChallengeCount = habit.getChallenges().size();
+            double maleCount = habit.getChallenges().stream().
+                    filter(challenge -> challenge.getUser().getGender() == MALE).count();
+            double femaleCount = habit.getChallenges().stream().
+                    filter(challenge -> challenge.getUser().getGender() == FEMALE).count();
+
+            this.male = (int) Math.round(maleCount / totalChallengeCount * 100);
+            this.female = (int) Math.round(femaleCount / totalChallengeCount * 100);
+        }
+        return this;
+    }
 }
