5e436bd888bfe16e22477245b42cf97eac366f66
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-02-02 17:29:21 +0900

    style - #382: 오타 수정

--- client/pages/habit/detail/[habitId]/index.tsx
+++ client/pages/habit/detail/[habitId]/index.tsx
@@ -68,8 +68,7 @@ const HabitDetail: React.FC = () => {
     });
   }, [router.isReady]);
 
-  // const progressValue = Math.ceil((habitData?.overview?.day / 66) * 100);
-  const progressValue = 60;
+  const progressValue = Math.ceil((habitData?.overview?.day / 66) * 100);
   const progress = progressValue > 100 ? 100 : progressValue;
   const succImgClassName = 'max-w-[50%]';
 
