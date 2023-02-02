891d8ead5a6d7cc45cacdbfe5e334d64d55ed936
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-02-03 08:26:23 +0900

    design - #382: z-index 조정

--- client/pages/user/guest.tsx
+++ client/pages/user/guest.tsx
@@ -2,17 +2,19 @@ import { LoadingIndicator } from '../../components/loadingIndicator';
 import { useRouter } from 'next/router';
 import { useAppDispatch } from '../../ducks/store';
 import { loginRequest } from '../../ducks/loginIdentitySlice';
+import { useEffect } from 'react';
 
 const GuestLogin = () => {
   const dispatch = useAppDispatch();
   const router = useRouter();
-
-  dispatch(
-    loginRequest({ username: 'guest@mail.com', password: 'guest123!' }),
-  ).then((data) => {
-    data.payload;
-    router.push('/');
-  });
+  useEffect(() => {
+    dispatch(
+      loginRequest({ username: 'guest@mail.com', password: 'guest123!' }),
+    ).then((data) => {
+      data.payload;
+      router.push('/');
+    });
+  }, []);
 
   return (
     <div className="h-screen flex items-center -mb-[100px]">
