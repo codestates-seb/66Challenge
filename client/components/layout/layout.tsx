13229fa4913999c1c71c3bfda6af21179f410bab
jwParkDev <110954895+jwParkDev@users.noreply.github.com>
2023-02-03 11:34:25 +0900

    design - #382: Background Image 삽입

--- client/components/layout/layout.tsx
+++ client/components/layout/layout.tsx
@@ -3,15 +3,20 @@ import { TopNav } from '../topNav';
 import { Header } from '../header';
 import { ScrollToTopButton } from '../scrollToTopButton';
 import React from 'react';
+import styled from 'styled-components';
+import Image from 'next/image';
 
 type LayoutProps = {
   children: React.ReactNode;
 };
 
+const DesktopDiv = styled.div``;
+
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="desktop-section bg-slate-100 flex justify-center">
-      <div className="app-section w-full min-w-[360px] max-w-[460px] bg-white">
+    <div className="desktop-section bg-slate-100 flex justify-center relative">
+      <div className="fixed insert-0 bg-mainBackground bg-no-repeat h-full w-full" />
+      <div className="app-section w-full min-w-[360px] max-w-[460px] bg-white z-[1]">
         <Header />
         <TopNav />
         <main className="pb-[100px] z-[1]">{children}</main>
