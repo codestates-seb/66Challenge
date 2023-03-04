02797c7015c5ce4cc66f9afedcacec815bdff968
Yerim Seonwoo <54367532+yerimsw@users.noreply.github.com>
2023-03-05 05:25:41 +0900

    Update WebSocketConfig.java

--- server/src/main/java/challenge/server/config/WebSocketConfig.java
+++ server/src/main/java/challenge/server/config/WebSocketConfig.java
@@ -11,7 +11,8 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws/chat").setAllowedOrigins("chrome-extension://ggnhohnkfcpcanfekomdkjffnfcjnjam").withSockJS();
+        String[] arr = {"chrome-extension://ggnhohnkfcpcanfekomdkjffnfcjnjam","https://66challenge.shop"};
+        registry.addEndpoint("/ws/chat").setAllowedOrigins(arr).withSockJS();
     }
 
     @Override
